173a071a6c3018b40af7c0796cf79f37355a12a9
Msiang <mou20181234@gmail.com>
2025-10-21 22:38:17 +0800

    Update AutoCountService.php

--- application/libraries/AutoCountService.php
+++ application/libraries/AutoCountService.php
@@ -164,7 +164,7 @@ class AutoCountService {
             $response = [
                 'status' => $httpCode,
                 //'headers' => $headersAssoc,
-                'body'   => (!empty($result)) ? $result : null,
+                'body'   => null,
                 'error'  => ($httpCode >= 400 ? "HTTP Error $httpCode" : null)
             ];
 
