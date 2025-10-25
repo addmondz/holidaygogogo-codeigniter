39f18559970a140c3c71832c370dfb18e98b5b6a
Msiang <77429710+chengmousiang@users.noreply.github.com>
2025-10-25 14:50:05 +0800

    Update 20251024_Supplier_Booking_Alter_Column.sql

--- application/sql/20251024_Supplier_Booking_Alter_Column.sql
+++ application/sql/20251024_Supplier_Booking_Alter_Column.sql
@@ -1,20 +1,8 @@
-
 ALTER TABLE supplier
-  ADD COLUMN AutocountSyncAction CHAR(1) NOT NULL DEFAULT 'C'
-  COMMENT 'C: Create, U: Update, D: Delete, V: Void, S: Update_Status'
-  AFTER Status;
-
-ALTER TABLE supplier
-  ADD COLUMN AutocountSyncStatus CHAR(1) NOT NULL DEFAULT 'P'
-  COMMENT 'P: Pending, S: Synced, F: Failed'
-  AFTER AutocountSyncAction;
-  
-ALTER TABLE supplier
-  ADD COLUMN AutocountSyncMessage TEXT AFTER AutocountSyncStatus;
-
-ALTER TABLE supplier
-  ADD COLUMN SupplierCode VARCHAR(255) NULL
-  AFTER Phone;
+  ADD COLUMN AutocountSyncAction CHAR(1) NOT NULL DEFAULT 'C' COMMENT 'C: Create, U: Update, D: Delete, V: Void, S: Update_Status' AFTER Status,
+  ADD COLUMN AutocountSyncStatus CHAR(1) NOT NULL DEFAULT 'P' COMMENT 'P: Pending, S: Synced, F: Failed' AFTER AutocountSyncAction,
+  ADD COLUMN AutocountSyncMessage TEXT AFTER AutocountSyncStatus,
+  ADD COLUMN SupplierCode VARCHAR(255) NULL AFTER Phone;
 
 CREATE INDEX IX_supplier_AutocountSyncAction ON supplier (AutocountSyncAction);
 CREATE INDEX IX_supplier_AutocountSyncStatus ON supplier (AutocountSyncStatus);
@@ -24,7 +12,7 @@ CREATE INDEX IX_supplier_SupplierCode ON supplier (SupplierCode);
 ALTER TABLE booking
   ADD COLUMN CustomerAutocountSyncAction CHAR(1) NOT NULL DEFAULT 'C'
   COMMENT 'C: Create, U: Update, D: Delete, V: Void, S: Update_Status'
-  AFTER Status;
+  AFTER `Status`;
 
 ALTER TABLE booking
   ADD COLUMN CustomerAutocountSyncStatus CHAR(1) NOT NULL DEFAULT 'P'
