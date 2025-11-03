ff144f560fcac295648d825c8e0ddacf6970aa23
Msiang <mou20181234@gmail.com>
2025-11-03 22:49:06 +0800

    Update Cron.php

--- application/controllers/Cron.php
+++ application/controllers/Cron.php
@@ -531,14 +531,14 @@ class Cron extends CI_Controller
 			}
 		}
 
-		// if (!empty($booking['CustomerID'])) {
-		// 	$customer = $this->Customer_Model->find($booking['CustomerID']);
-		// 	if (!empty($customer)) {
-		// 		if (!empty($customer['CustomerCode'])) {
-		// 			$booking['CustomerCode'] = $customer['CustomerCode'];
-		// 		}
-		// 	}
-		// }
+		if (!empty($booking['CustomerID'])) {
+			$customer = $this->Customer_Model->find($booking['CustomerID']);
+			if (!empty($customer)) {
+				if (!empty($customer->CustomerCode) && $customer->CustomerCode != null) {
+					$booking['CustomerCode'] = $customer->CustomerCode;
+				}
+			}
+		}
 
 		return $booking;
 	}
