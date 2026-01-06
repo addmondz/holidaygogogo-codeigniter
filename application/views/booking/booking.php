2e1a9287f4a4f9b89c4ded25d95e50c6c7a2965f
yx13 <exian97@gmail.com>
2026-01-06 14:48:20 +0800

    hide total summary on booking deatil for SA

--- application/views/booking/booking.php
+++ application/views/booking/booking.php
@@ -586,6 +586,8 @@
 
                     <br><br>
 
+                    <?php if($this->session->userdata('level') != 20) { ?>
+
                     <div class="row">
 
                         <div class="col-md-12 pt-3 pb-3" style="background-color:white; border:3px solid #D7E2F2; border-radius:8px;">
@@ -651,6 +653,8 @@
                         </div>
 
                     </div>
+
+                    <?php } ?>
 
                     <br>
 
