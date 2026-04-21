a3056b51362771ee85fe1bf8710a7915aed3bcea
Edmond Lang <edmond_wuiming@hotmail.com>
2026-04-21 22:04:30 +0800

    code update

--- application/controllers/Cron.php
+++ application/controllers/Cron.php
@@ -332,10 +332,10 @@ class Cron extends CI_Controller
 
 	public function process_ghl_leads($chunkSize = 100)
 	{
-		// if (!$this->input->is_cli_request()) {
-		// 	show_error('This script can only be run from the command line.', 403);
-		// 	return;
-		// }
+		if (!$this->input->is_cli_request()) {
+			show_error('This script can only be run from the command line.', 403);
+			return;
+		}
 
 		$this->load->model('Ghl_Processed_Leads_Model');
 
@@ -1476,10 +1476,10 @@ class Cron extends CI_Controller
 	 */
 	public function syncGhlUsers()
 	{
-		// if (!$this->input->is_cli_request()) {
-		// 	show_error('Not allowed', 403);
-		// 	return;
-		// }
+		if (!$this->input->is_cli_request()) {
+			show_error('Not allowed', 403);
+			return;
+		}
 
 		$this->load->library('GhlUsersSyncService');
 		$result = $this->ghluserssyncservice->sync();
@@ -1494,10 +1494,10 @@ class Cron extends CI_Controller
 	 */
 	public function syncGhlContacts()
 	{
-		// if (!$this->input->is_cli_request()) {
-		// 	show_error('Not allowed', 403);
-		// 	return;
-		// }
+		if (!$this->input->is_cli_request()) {
+			show_error('Not allowed', 403);
+			return;
+		}
 
 		$args = isset($_SERVER['argv']) ? $_SERVER['argv'] : array();
 		$uriSegments = $this->uri->segment_array();
@@ -1521,10 +1521,10 @@ class Cron extends CI_Controller
 	 */
 	public function syncGhlConversations()
 	{
-		// if (!$this->input->is_cli_request()) {
-		// 	show_error('Not allowed', 403);
-		// 	return;
-		// }
+		if (!$this->input->is_cli_request()) {
+			show_error('Not allowed', 403);
+			return;
+		}
 
 		$args = isset($_SERVER['argv']) ? $_SERVER['argv'] : array();
 		$uriSegments = $this->uri->segment_array();
@@ -1548,10 +1548,10 @@ class Cron extends CI_Controller
 	 */
 	public function syncGhlMessages()
 	{
-		// if (!$this->input->is_cli_request()) {
-		// 	show_error('Not allowed', 403);
-		// 	return;
-		// }
+		if (!$this->input->is_cli_request()) {
+			show_error('Not allowed', 403);
+			return;
+		}
 
 		$args = isset($_SERVER['argv']) ? $_SERVER['argv'] : array();
 		$uriSegments = $this->uri->segment_array();
